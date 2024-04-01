abf5a1d29c408c560098df029b64e2941caecc09
Christina Repou <54071776+ChrRepou@users.noreply.github.com>
2024-04-01 10:31:27 +0000

    fixing a small issue in wordpress script

--- requirements/wordpress/create_page.sh
+++ requirements/wordpress/create_page.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 
-if [ -f "/var/www/html/wordpress/wp-config.php" ]
+WORDPRESS_DIR="/home/christina/data/html"
 
-then
-  echo "Wordpress is already confiured."
+if [ -f "$WORDPRESS_DIR/wp-config.php" ]; then
+  echo "WordPress is already configured."
 else
+  # Your existing setup commands here
   wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
   chmod +x wp-cli.phar
   mv wp-cli.phar /usr/local/bin/wp
-  wp core download --path=/var/www/html/wordpress --allow-root
-  wp config create --dbname=wordpress --dbuser=crepou --dbpass=pass --dbhost=mariadb --path=/var/www/html/wordpress --skip-check --allow-root
-  wp core install --path=/var/www/html/wordpress --url=crepou.42.fr --title=Inception --admin_user=ChristinaRepou --admin_password=ChristinaRepou --admin_email=crepou@student.42heilbronn.de --skip-email --allow-root
-  wp theme install teluro --path=/var/www/html/wordpress --activate --allow-root
-  wp user create leon leon@le.on --role=author --path=/var/www/html/wordpress --user_pass=leon --allow-root
-
+  wp core download --path="$WORDPRESS_DIR" --allow-root
+  wp config create --dbname=wordpress --dbuser=crepou --dbpass=pass --dbhost=mariadb --path="$WORDPRESS_DIR" --skip-check --allow-root
+  wp core install --path="$WORDPRESS_DIR" --url=crepou.42.fr --title=Inception --admin_user=ChristinaRepou --admin_password=ChristinaRepou --admin_email=crepou@student.42heilbronn.de --skip-email --allow-root
+  wp theme install teluro --path="$WORDPRESS_DIR" --activate --allow-root
+  wp user create leon leon@le.on --role=author --path="$WORDPRESS_DIR" --user_pass=leon --allow-root
 fi
 
 /usr/sbin/php-fpm7.3 -F
