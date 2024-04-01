37c88e1590f69972c7e88f2f46077094e7c2362d
Christina Repou <54071776+ChrRepou@users.noreply.github.com>
2024-04-01 10:23:31 +0000

    fixing a small issue in wordpress script

--- requirements/wordpress/create_page.sh
+++ requirements/wordpress/create_page.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [ -f "/var/www/wordpress/wp-config.php" ]
+if [ -f "/var/www/html/wordpress/wp-config.php" ]
 
 then
   echo "Wordpress is already confiured."
@@ -8,11 +8,11 @@ else
   wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
   chmod +x wp-cli.phar
   mv wp-cli.phar /usr/local/bin/wp
-  wp core download --path=/var/www/wordpress --allow-root
-  wp config create --dbname=wordpress --dbuser=crepou --dbpass=pass --dbhost=mariadb --path=/var/www/wordpress --skip-check --allow-root
-  wp core install --path=/var/www/wordpress --url=crepou.42.fr --title=Inception --admin_user=ChristinaRepou --admin_password=ChristinaRepou --admin_email=crepou@student.42heilbronn.de --skip-email --allow-root
-  wp theme install teluro --path=/var/www/wordpress --activate --allow-root
-  wp user create leon leon@le.on --role=author --path=/var/www/wordpress --user_pass=leon --allow-root
+  wp core download --path=/var/www/html/wordpress --allow-root
+  wp config create --dbname=wordpress --dbuser=crepou --dbpass=pass --dbhost=mariadb --path=/var/www/html/wordpress --skip-check --allow-root
+  wp core install --path=/var/www/html/wordpress --url=crepou.42.fr --title=Inception --admin_user=ChristinaRepou --admin_password=ChristinaRepou --admin_email=crepou@student.42heilbronn.de --skip-email --allow-root
+  wp theme install teluro --path=/var/www/html/wordpress --activate --allow-root
+  wp user create leon leon@le.on --role=author --path=/var/www/html/wordpress --user_pass=leon --allow-root
 
 fi
 
