4cfccbd6119fd379f20a256c814fb2647e6e99a5
Christina Repou <54071776+ChrRepou@users.noreply.github.com>
2024-04-01 10:52:25 +0000

    put all files inside srcs folder, creating an .env file and make some updates in the docker-compose

--- srcs/requirements/wordpress/create_page.sh
+++ srcs/requirements/wordpress/create_page.sh
@@ -11,7 +11,7 @@ else
   mv wp-cli.phar /usr/local/bin/wp
   wp core download --path="$WORDPRESS_DIR" --allow-root
   wp config create --dbname=wordpress --dbuser=crepou --dbpass=pass --dbhost=mariadb --path="$WORDPRESS_DIR" --skip-check --allow-root
-  wp core install --path="$WORDPRESS_DIR" --url=crepou.42.fr --title=Inception --admin_user=ChristinaRepou --admin_password=ChristinaRepou --admin_email=crepou@student.42heilbronn.de --skip-email --allow-root
+  wp core install --path="$WORDPRESS_DIR" --url=crepou.42.fr --title=Inception --admin_user=ChristinaRepou --admin_password=ChristinaRepou --admin_email=@student.42heilbronn.de --skip-email --allow-root
   wp theme install teluro --path="$WORDPRESS_DIR" --activate --allow-root
   wp user create leon leon@le.on --role=author --path="$WORDPRESS_DIR" --user_pass=leon --allow-root
 fi
