243a1faa25dc356d7be7a3a7449a7f5d4d1bfec4
Christina Repou <54071776+ChrRepou@users.noreply.github.com>
2024-04-02 19:33:41 +0000

    fixing variables error

--- srcs/requirements/wordpress/tools/create_page.sh
+++ srcs/requirements/wordpress/tools/create_page.sh
@@ -10,11 +10,11 @@ else
   wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
   chmod +x wp-cli.phar
   mv wp-cli.phar /usr/local/bin/wp
-  wp core download --path=$WORDPESS_PATH --allow-root
-  wp config create --dbname=$MYSQL_DB --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASS --dbhost=mariadb --path=$WORDPESS_PATH --skip-check --allow-root
-  wp core install --path=$WORDPESS_PATH --url=$DOMAIN --title=$WORDPRESS_TITLE --admin_user=$WORDPRESS_USER --admin_password=$WORDPRESS_PASS --admin_email=$WORDPRESS_EMAIL --skip-email --allow-root
-  wp theme install teluro --path=$WORDPESS_PATH --activate --allow-root
-  wp user create leon leon@le.on --role=author --path=$WORDPESS_PATH --user_pass=leon --allow-root
+  wp core download --path=${WORDPESS_PATH} --allow-root
+  wp config create --dbname=${MYSQL_DB} --dbuser=${MYSQL_USER} --dbpass=${MYSQL_PASS} --dbhost=mariadb --path=${WORDPESS_PATH} --skip-check --allow-root
+  wp core install --path=${WORDPESS_PATH} --url=${DOMAIN} --title=${WORDPRESS_TITLE} --admin_user=${WORDPRESS_USER} --admin_password=${WORDPRESS_PASS} --admin_email=${WORDPRESS_EMAIL} --skip-email --allow-root
+  wp theme install teluro --path=${WORDPESS_PATH} --activate --allow-root
+  wp user create leon leon@le.on --role=author --path=${WORDPESS_PATH} --user_pass=leon --allow-root
 
 fi
 
