313dd97ef7e6cc95bf0cd79b7c926b0982bccacb
Christina Repou <54071776+ChrRepou@users.noreply.github.com>
2024-04-06 20:37:01 +0000

    making some changes

--- srcs/requirements/wordpress/tools/create_page.sh
+++ srcs/requirements/wordpress/tools/create_page.sh
@@ -1,21 +1,18 @@
 #!/bin/sh
 
-sed -i 's|PHP_PORT|'${PHP_PORT}'|g' /etc/php/7.3/fpm/pool.d/www.conf
+#sed -i 's|PHP_PORT|'${PHP_PORT}'|g' /etc/php/7.3/fpm/pool.d/www.conf
+sed -i "s/listen = 127.0.0.1:9000/listen = 9000/g" /etc/php81/php-fpm.d/www.conf
 
-if [ -f "/home/christina/data/html/wp-config.php" ]
+  #wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+  #chmod +x wp-cli.phar
+  #mv wp-cli.phar /usr/local/bin/wp
+  #wp core download --path=${WORDPRESS_PATH} --allow-root
+  sleep 60
+  wp config create --allow-root --dbname=${MYSQL_DB} --dbuser=${MYSQL_USER} --dbpass=${MYSQL_PASS} --dbhost=mariadb:3306 --url=${DOMAIN} --force #--path=${WORDPRESS_PATH} --skip-check 
+  wp core install --allow-root --url=${DOMAIN} --title=${WORDPRESS_TITLE} --admin_user=${WORDPRESS_USER} --admin_password=${WORDPRESS_PASS} --admin_email=${WORDPRESS_EMAIL}
+  wp user create ${WP_USER} ${WP_USER_EMAIL} --user_pass=${WP_USER_PASS} --allow-root
+  wp option update home ${DOMAIN}
+  wp option update siteurl ${DOMAIN}
 
-then
-  echo "Wordpress already configured."
-else
-  wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-  chmod +x wp-cli.phar
-  mv wp-cli.phar /usr/local/bin/wp
-  wp core download --path=${WORDPRESS_PATH} --allow-root
-  wp config create --dbname=${MYSQL_DB} --dbuser=${MYSQL_USER} --dbpass=${MYSQL_PASS} --dbhost=mariadb --path=${WORDPRESS_PATH} --skip-check --allow-root
-  wp core install --path=${WORDPRESS_PATH} --url=${DOMAIN} --title=${WORDPRESS_TITLE} --admin_user=${WORDPRESS_USER} --admin_password=${WORDPRESS_PASS} --admin_email=${WORDPRESS_EMAIL} --skip-email --allow-root
-  wp theme install teluro --path=${WORDPRESS_PATH} --activate --allow-root
-  wp user create leon leon@le.on --role=author --path=${WORDPRESS_PATH} --user_pass=leon --allow-root
 
-fi
-
-/usr/sbin/php-fpm7.3 -F
+php-fpm81 -F
