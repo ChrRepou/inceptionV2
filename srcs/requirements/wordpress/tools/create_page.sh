c8ea0f61170878084d8cc536687409381e33ca87
Christina Repou <54071776+ChrRepou@users.noreply.github.com>
2024-04-07 15:19:20 +0000

    restoring old script

--- srcs/requirements/wordpress/tools/create_page.sh
+++ srcs/requirements/wordpress/tools/create_page.sh
@@ -1,21 +1,22 @@
 #!/bin/sh
 
-sed -i "s/;listen = 127.0.0.1:9000/listen = 127.0.0.1:9000/g" /etc/php81/php-fpm.d/www.conf
+#sed -i 's|PHP_PORT|'${PHP_PORT}'|g' /etc/php81/php-fpm.d/www.conf
+sed -i "s/listen = 127.0.0.1/listen = 127.0.0.1:9000/g" /etc/php81/php-fpm.d/www.conf
 
-echo "[WORDPRESS] Waiting for MariaDB..."
+echo "[WP config] Waiting for MariaDB..."
 while ! mariadb -h${DB_HOST} -u${MYSQL_USER} -p${MYSQL_PASS} ${MYSQL_DB} &>/dev/null;
 do
     sleep 3
 done
-echo "[WORDPRESS] MariaDB accessible."
-sleep 30
+echo "[WP config] MariaDB accessible."
 
-echo "[WORDPRESS] Creating users..."
+
+sleep 60
 wp config create --allow-root --dbname=${MYSQL_DB} --dbuser=${MYSQL_USER} --dbpass=${MYSQL_PASS} --dbhost=mariadb:3306 --url=${DOMAIN} --force #--path=${WORDPRESS_PATH} --skip-check 
 wp core install --allow-root --url=${DOMAIN} --title=${WORDPRESS_TITLE} --admin_user=${MYSQL_USER} --admin_password=${MYSQL_ROOT_PASS} --admin_email=${WORDPRESS_ADMIN_EMAIL}
 wp user create ${WORDPRESS_USER} ${WORDPRESS_EMAIL} --user_pass=${WORDPRESS_PASS} --allow-root
 wp option update home ${DOMAIN}
 wp option update siteurl ${DOMAIN}
-echo "[WORDPRESS] Set up ready!"
 
+#php-fpm81 -F
 exec /usr/sbin/php-fpm81 -F -R
