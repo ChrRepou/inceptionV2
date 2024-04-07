aafdeb77c149ad35421e6a2d8fddd04cb82a9a27
Christina Repou <54071776+ChrRepou@users.noreply.github.com>
2024-04-07 12:09:13 +0000

    making some changes in the script of the wordpess

--- srcs/requirements/wordpress/tools/create_page.sh
+++ srcs/requirements/wordpress/tools/create_page.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 
-sed -i 's|PHP_PORT|'${PHP_PORT}'|g' /etc/php81/php-fpm.d/www.conf
-#sed -i "s/listen = 127.0.0.1:9000/listen = 9000/g" /etc/php81/php-fpm.d/www.conf
+#sed -i 's|PHP_PORT|'${PHP_PORT}'|g' /etc/php81/php-fpm.d/www.conf
+sed -i "s/listen = 127.0.0.1:9000/listen = 9000/g" /etc/php81/php-fpm.d/www.conf
+
+echo "[WP config] Waiting for MariaDB..."
+while ! mariadb -h${DB_HOST} -u${WP_DB_USER} -p${WP_DB_PASS} ${WP_DB_NAME} &>/dev/null;
+do
+    sleep 3
+done
+echo "[WP config] MariaDB accessible."
+
 
 sleep 60
 wp config create --allow-root --dbname=${MYSQL_DB} --dbuser=${MYSQL_USER} --dbpass=${MYSQL_PASS} --dbhost=mariadb:3306 --url=${DOMAIN} --force #--path=${WORDPRESS_PATH} --skip-check 
@@ -10,4 +18,5 @@ wp user create ${WORDPRESS_USER} ${WORDPRESS_EMAIL} --user_pass=${WORDPRESS_PASS
 wp option update home ${DOMAIN}
 wp option update siteurl ${DOMAIN}
 
-php-fpm81 -F
+#php-fpm81 -F
+exec /usr/sbin/php-fpm81 -F -R
