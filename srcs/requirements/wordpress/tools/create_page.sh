a3b420737efede48328b7d7c28e917d2d5a234ec
Christina Repou <54071776+ChrRepou@users.noreply.github.com>
2024-04-07 11:46:55 +0000

    fixing variable names in the wordpress script

--- srcs/requirements/wordpress/tools/create_page.sh
+++ srcs/requirements/wordpress/tools/create_page.sh
@@ -9,7 +9,7 @@ sed -i "s/listen = 127.0.0.1:9000/listen = 9000/g" /etc/php81/php-fpm.d/www.conf
   #wp core download --path=${WORDPRESS_PATH} --allow-root
   sleep 60
   wp config create --allow-root --dbname=${MYSQL_DB} --dbuser=${MYSQL_USER} --dbpass=${MYSQL_PASS} --dbhost=mariadb:3306 --url=${DOMAIN} --force #--path=${WORDPRESS_PATH} --skip-check 
-  wp core install --allow-root --url=${DOMAIN} --title=${WORDPRESS_TITLE} --admin_user=${WORDPRESS_USER} --admin_password=${WORDPRESS_PASS} --admin_email=${WORDPRESS_EMAIL}
+  wp core install --allow-root --url=${DOMAIN} --title=${WORDPRESS_TITLE} --admin_user=${MYSQL_USER} --admin_password=${MYSQL_ROOT_PASS} --admin_email=${WORDPRESS_ADMIN_EMAIL}
   wp user create ${WORDPRESS_USER} ${WORDPRESS_EMAIL} --user_pass=${WORDPRESS_PASS} --allow-root
   wp option update home ${DOMAIN}
   wp option update siteurl ${DOMAIN}
