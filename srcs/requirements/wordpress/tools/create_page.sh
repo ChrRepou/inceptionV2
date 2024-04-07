03d55c8787991bff99a4c715be1a99854d7110da
Christina Repou <54071776+ChrRepou@users.noreply.github.com>
2024-04-07 11:54:56 +0000

    fixing bug to wordpress script

--- srcs/requirements/wordpress/tools/create_page.sh
+++ srcs/requirements/wordpress/tools/create_page.sh
@@ -1,18 +1,13 @@
 #!/bin/sh
 
 #sed -i 's|PHP_PORT|'${PHP_PORT}'|g' /etc/php81/php-fpm.d/www.conf
-sed -i "s/listen = 127.0.0.1:9000/listen = 9000/g" /etc/php81/php-fpm.d/www.conf
+#sed -i "s/listen = 127.0.0.1:9000/listen = 9000/g" /etc/php81/php-fpm.d/www.conf
 
-  #wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-  #chmod +x wp-cli.phar
-  #mv wp-cli.phar /usr/local/bin/wp
-  #wp core download --path=${WORDPRESS_PATH} --allow-root
-  sleep 60
-  wp config create --allow-root --dbname=${MYSQL_DB} --dbuser=${MYSQL_USER} --dbpass=${MYSQL_PASS} --dbhost=mariadb:3306 --url=${DOMAIN} --force #--path=${WORDPRESS_PATH} --skip-check 
-  wp core install --allow-root --url=${DOMAIN} --title=${WORDPRESS_TITLE} --admin_user=${MYSQL_USER} --admin_password=${MYSQL_ROOT_PASS} --admin_email=${WORDPRESS_ADMIN_EMAIL}
-  wp user create ${WORDPRESS_USER} ${WORDPRESS_EMAIL} --user_pass=${WORDPRESS_PASS} --allow-root
-  wp option update home ${DOMAIN}
-  wp option update siteurl ${DOMAIN}
-
+sleep 60
+wp config create --allow-root --dbname=${MYSQL_DB} --dbuser=${MYSQL_USER} --dbpass=${MYSQL_PASS} --dbhost=mariadb:3306 --url=${DOMAIN} --force #--path=${WORDPRESS_PATH} --skip-check 
+wp core install --allow-root --url=${DOMAIN} --title=${WORDPRESS_TITLE} --admin_user=${MYSQL_USER} --admin_password=${MYSQL_ROOT_PASS} --admin_email=${WORDPRESS_ADMIN_EMAIL}
+wp user create ${WORDPRESS_USER} ${WORDPRESS_EMAIL} --user_pass=${WORDPRESS_PASS} --allow-root
+wp option update home ${DOMAIN}
+wp option update siteurl ${DOMAIN}
 
 php-fpm81 -F
